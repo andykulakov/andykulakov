7bdeda55e55094f5afbab4631aa0c632b2de3d9d
Andrey <andykulakov@gmail.com>
2020-08-08 19:32:16 +0300

    Added UglifyJS for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,18 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.config.common.js');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.config.common.js");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    })
-  ]
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
 });
